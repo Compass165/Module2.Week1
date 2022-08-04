2b11f9777839dbea857abe94ec2335efe9ae46c4
Compass <ndphong165@gmail.com>
2022-08-04 23:36:11 +0700

    Up thuc hanh lai suat

---
+++ 2.LoopOfJava/PT/src/LaiSuatNam.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LaiSuatNam {
+    public static void main(String[] args) {
+
+        double money = 1.0;
+        int month = 1;
+        double interestRate = 1.0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Nhập số tiền bạn gửi: ");
+        money = input.nextDouble();
+        System.out.println("Nhập số tháng bạn muốn gửi: ");
+        month = input.nextInt();
+        System.out.println("Nhập phần trăm lãi suất được nhận: ");
+        interestRate = input.nextDouble();
+
+        double totalInterest = 0;
+        for (int i=0; i<month; i++){;
+            totalInterest += money * (interestRate/100)/12 * month;
+        }
+        System.out.println("totalInterest = " + totalInterest);
+    }
+}
