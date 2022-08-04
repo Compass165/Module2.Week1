9cde9948ce147252856ac327eeec014da218001c
Compass <ndphong165@gmail.com>
2022-08-04 14:58:35 +0700

    Đã lam het bai thuc hanh

--- 1.IntroductionToJava/LuyenTap/src/PT1.java
+++ 1.IntroductionToJava/LuyenTap/src/PT1.java
@@ -1,7 +1,26 @@
 import java.util.Date;
 public class PT1 {
-    public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println("Now is: " + now);
-    }
+//    public static void main(String[] args) {
+////        Date now = new Date();
+////        System.out.println("Now is: " + now);
+//        double x = 45;
+//        if (x >= 50) {
+//            System.out.println("CODE");
+//        }
+//        System.out.println("GYM");
+//    }
+//    }
+//public void method(int x) {
+//
+//    int y = 5;
+//
+//    if (x > y) {
+//
+//        int z = x + y;
+//
+//    }
+//
+//    System.out.print(z);
+//}
+
 }
