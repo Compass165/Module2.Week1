6c9d9376768fc32847f69fc3f89582ae7bbd78f5
Compass <ndphong165@gmail.com>
2022-08-05 08:58:40 +0700

    Thêm phần label để break vong lap bai demo, Loop Java

--- 2.LoopOfJava/Demo/src/Demo.java
+++ 2.LoopOfJava/Demo/src/Demo.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Demo {
+import java.util.Scanner;
+
+public class Demo {
+//Demo break
+//    public static void main(String[] args) {
+//        for (int i=0; i<10;i++) {
+//            if (i==5)
+//                break;
+//            System.out.printf("%d\t",i);
+//        }
+//        System.out.printf("%s\t","End of Loop");
+//    }
+
+//    Demo continue
+    public static void main(String[] args) {
+//        for (int i=0; i<10;i++) {
+//            if (i==5)
+//                continue;
+//            System.out.printf("%d\t",i);
+//        }
+//        System.out.printf("%s\t","End of Loop");
+        int a[] = {3, 4, 5, 7, 8, 9};
+
+        a: for (int i = 10; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j==4) break a;
+                System.out.println(i+ " "+ j);
+            }
+
+        }
+
+    }
+
+
+
 }
--- 2.LoopOfJava/Demo/src/Demo2.java
+++ 2.LoopOfJava/Demo/src/Demo2.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Demo2 {
+public class Demo2 {
+
+    public static void main(String[] args) {
+//        System.out.println("Multiplication Table");
+//
+//        System.out.print(" ");
+//        for (int i = 1; i <= 9; i++) {
+//            System.out.print(i + " | ");
+//            for (int j = 1; j <= 9; j++) {
+//                // Display the product and align properly
+//                System.out.printf("%4d", i * j);
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 0; i < 10000; i++)
+//            for (int j = 0; j < 10000; j++)
+//                for (int k = 0; k < 10000; k++)
+//                    Perform an action
+//
+//    }
+    }
 }
